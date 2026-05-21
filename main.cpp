7eedc34cd366461da4709549e6a286882a9c4718
Roland Baker <feive7@gmail.com>
2026-05-21 13:52:01 -0700

    main cleanup

--- main.cpp
+++ main.cpp
@@ -15,32 +15,11 @@ int main(int argc, char** argv) {
     SetTargetFPS(60);
 
     // Create scene
-    PhysLayer* physlayer = new PhysLayer();
-    physlayer->setPhysicsScale(0.05f);
-    physlayer->setGravity({0.0f,9.0f});
-    physlayer->reparent(scene);
-
-    PhysBox* box = new PhysBox();
-    box->setPosition(530.0f,200.0f);
-    box->size = {70.0f,70.0f};
-    box->reparent(scene);
-    box->setFixed(false);
-    physlayer->add(box);
-
-    PhysBox* ground = new PhysBox();
-    ground->setPosition(400.0f,300.0f);
-    ground->size = {100.0f,5.0f};
-    ground->reparent(scene);
-    ground->setFixed(true);
-    physlayer->add(ground);
-
-    Sprite2D* sprite = new Sprite2D("../assets/funi.jpg");
-    sprite->size = box->size;
-    sprite->reparent(box);
-
-    SolidBox* ground_sprite = new SolidBox();
-    ground_sprite->size = ground->size;
-    ground_sprite->reparent(ground);
+    Sprite2D* sprite = new Sprite2D();
+    sprite->setPosition({400.0f,225.0f});
+    sprite->size = {100.0f,100.0f};
+    sprite->setTexture("../assets/funi.jpg");
+    sprite->reparent(scene);
 
     // Initialize scene tree
     ReadyTree();
@@ -53,7 +32,6 @@ int main(int argc, char** argv) {
         ClearBackground(DARKGRAY);
         DrawTree();
         DrawFPS(5, 5);
-        DrawText(TextFormat("Sprite Pos: %f %f",box->position.x,box->position.y),5,25,20,WHITE);
         EndDrawing();
     }
 
