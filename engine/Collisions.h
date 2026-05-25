4d55a368daa1f80fc0bd1d1fb1980fef32ce8606
rolan <rolan@DESKTOP-MOG1UQ9>
2026-05-25 10:10:47 -0700

    formatting

--- engine/Collisions.h
+++ engine/Collisions.h
@@ -1,59 +1,134 @@
 #pragma once
 
 enum CollisionShapeType : int {
-    None,
-    Box,
-    Circle
+	None,
+	Box,
+	Circle
 };
 
 class CollisionShape : public Object2D {
 protected:
-    CollisionShapeType shape = CollisionShapeType::None;
+	CollisionShapeType shape = CollisionShapeType::None;
 public:
-    virtual bool contains(Vector2 point) = 0;
-    virtual bool overlapping(CollisionShape* other) = 0;
-    inline CollisionShapeType getShape() { return shape; }
+	virtual bool contains(Vector2 point) = 0;
+	virtual bool overlapping(CollisionShape* other) = 0;
+	inline CollisionShapeType getShape() { return shape; }
 };
 
 class CollisionBox : public CollisionShape {
 public:
-    Vector2 size;
-    CollisionBox() {
-        this->shape = CollisionShapeType::Box;
-    }
-    bool contains(Vector2 point) override {
-        // Localize point
-        point -= getGlobalPosition();
-        point = Vector2Rotate(point,getGlobalRotation()*DEG2RAD);
+	Vector2 size;
+	CollisionBox() {
+		this->shape = CollisionShapeType::Box;
+	}
+	bool contains(Vector2 point) override {
+		// Localize point
+		point -= getGlobalPosition();
+		point = Vector2Rotate(point, getGlobalRotation() * DEG2RAD);
 
-        Vector2 gscl = getGlobalScale() * size;
-        return point.x > -gscl.x && point.x < gscl.x && point.y > -gscl.y && point.y < gscl.y;
-    }
-    bool overlapping(CollisionShape* other) override;
+		Vector2 gscl = getGlobalScale() * size;
+		return point.x > -gscl.x && point.x < gscl.x && point.y > -gscl.y && point.y < gscl.y;
+	}
+	bool overlapping(CollisionShape* other) override;
 };
 
 class CollisionCircle : public CollisionShape {
 public:
-    float radius;
-    CollisionCircle() {
-        this->shape = CollisionShapeType::Circle;
-    }
-    bool contains(Vector2 point) override {
-        // Localize point
-        point -= getGlobalPosition();
+	float radius;
+	CollisionCircle() {
+		this->shape = CollisionShapeType::Circle;
+	}
+	bool contains(Vector2 point) override {
+		// Localize point
+		point -= getGlobalPosition();
 
-        float grad = getGlobalScale().x * radius;
-        return hypot(point.x,point.y) < grad;
-    }
-    bool overlapping(CollisionShape* other) override;
+		float grad = getGlobalScale().x * radius;
+		return hypot(point.x, point.y) < grad;
+	}
+	bool overlapping(CollisionShape* other) override;
 };
 
+bool TestAxis(Vector2 axis, std::vector<Vector2> verts_a, std::vector<Vector2> verts_b) {
+	float min_od_a = FLT_MAX;
+	float max_od_a = -FLT_MAX;
+	for (const Vector2& vert : verts_a) {
+		float od = Vector2DotProduct(vert, axis);
+		min_od_a = fmin(min_od_a, od);
+		max_od_a = fmax(max_od_a, od);
+	}
+
+	float min_od_b = FLT_MAX;
+	float max_od_b = -FLT_MAX;
+	for (const Vector2& vert : verts_b) {
+		float od = Vector2DotProduct(vert, axis);
+		min_od_b = fmin(min_od_b, od);
+		max_od_b = fmax(max_od_b, od);
+	}
+
+	return !(max_od_a < min_od_b || max_od_b < min_od_a);
+}
+
 bool CollisionBox::overlapping(CollisionShape* other) {
-    CollisionBox* other_box = dynamic_cast<CollisionBox*>(other);
-    Vector2 v11 = Vector2Rotate();
-    return false;
+	Vector2 this_gpos = getGlobalPosition();
+	float this_grot = getGlobalRotation();
+	Vector2 this_gscl = getGlobalScale() * size;
+	CollisionShapeType other_shape = other->getShape();
+	if (other_shape == CollisionShapeType::Box) {
+		CollisionBox* other_box = dynamic_cast<CollisionBox*>(other);
+		Vector2 other_gpos = other_box->getGlobalPosition();
+		float other_grot = other_box->getGlobalRotation();
+		Vector2 other_gscl = other_box->getGlobalScale() * other_box->size;
+
+
+		Vector2 box_a_normal = { cos(this_grot * -DEG2RAD),sin(this_grot * -DEG2RAD) };
+		Vector2 box_b_normal = { cos(other_grot * -DEG2RAD),sin(other_grot * -DEG2RAD) };
+
+		Vector2 box_a_1 = Vector2Rotate(this_gscl, this_grot * -DEG2RAD) + this_gpos;
+		Vector2 box_a_2 = Vector2Rotate(this_gscl * Vector2{ -1,1 }, this_grot * -DEG2RAD) + this_gpos;
+		Vector2 box_a_3 = Vector2Rotate(this_gscl * Vector2{ -1,-1 }, this_grot * -DEG2RAD) + this_gpos;
+		Vector2 box_a_4 = Vector2Rotate(this_gscl * Vector2{ 1,-1 }, this_grot * -DEG2RAD) + this_gpos;
+
+		Vector2 box_b_1 = Vector2Rotate(other_gscl, other_grot * -DEG2RAD) + other_gpos;
+		Vector2 box_b_2 = Vector2Rotate(other_gscl * Vector2{ -1,1 }, other_grot * -DEG2RAD) + other_gpos;
+		Vector2 box_b_3 = Vector2Rotate(other_gscl * Vector2{ -1,-1 }, other_grot * -DEG2RAD) + other_gpos;
+		Vector2 box_b_4 = Vector2Rotate(other_gscl * Vector2{ 1,-1 }, other_grot * -DEG2RAD) + other_gpos;
+
+		Vector2 axis_1 = box_a_normal;
+		Vector2 axis_2 = box_b_normal;
+		Vector2 axis_3 = Vector2{ -box_a_normal.y,box_a_normal.x };
+		Vector2 axis_4 = Vector2{ -box_b_normal.y,box_b_normal.x };
+
+		if (!TestAxis(axis_1, { box_a_1,box_a_2,box_a_3,box_a_4 }, { box_b_1,box_b_2,box_b_3,box_b_4 })) return false;
+		if (!TestAxis(axis_2, { box_a_1,box_a_2,box_a_3,box_a_4 }, { box_b_1,box_b_2,box_b_3,box_b_4 })) return false;
+		if (!TestAxis(axis_3, { box_a_1,box_a_2,box_a_3,box_a_4 }, { box_b_1,box_b_2,box_b_3,box_b_4 })) return false;
+		if (!TestAxis(axis_4, { box_a_1,box_a_2,box_a_3,box_a_4 }, { box_b_1,box_b_2,box_b_3,box_b_4 })) return false;
+
+		return true;
+	}
+	else if (other_shape == CollisionShapeType::Circle) {
+		CollisionCircle* other_circle = dynamic_cast<CollisionCircle*>(other);
+		Vector2 other_gpos = Vector2Rotate(other_circle->getGlobalPosition() - this_gpos, this_grot * DEG2RAD);
+		float other_grad = other_circle->getGlobalScale().x * other_circle->radius;
+
+		// SDF method
+		Vector2 d = Vector2{ abs(other_gpos.x),abs(other_gpos.y) } - this_gscl;
+		return Vector2Length(Vector2{ fmax(d.x,0.0f),fmax(d.y,0.0f) }) + fmin(fmax(d.x, d.y), 0.0f) < other_grad;
+	}
+	return false;
 }
 
 bool CollisionCircle::overlapping(CollisionShape* other) {
-    return false;
+	Vector2 this_gpos = getGlobalPosition();
+	float this_grad = getGlobalScale().x * radius;
+	CollisionShapeType other_shape = other->getShape();
+	if (other_shape == CollisionShapeType::Box) {
+		return other->overlapping(this);
+	}
+	else if (other->getShape() == CollisionShapeType::Circle) {
+		CollisionCircle* other_circle = dynamic_cast<CollisionCircle*>(other);
+		Vector2 other_gpos = other_circle->getGlobalPosition();
+		float other_grad = other_circle->getGlobalScale().x * other_circle->radius;
+		return Vector2DistanceSqr(this_gpos, other_gpos) < this_grad * this_grad + other_grad * other_grad;
+	}
+	return false;
 }
--- main.cpp
+++ main.cpp
@@ -4,44 +4,74 @@
 #include "rlgl.h"
 
 #include "engine/Engine.h"
+void DrawPoint(Vector2 p, Color color = BLACK) {
+	DrawCircleV(p, 3.0f, color);
+}
+void DebugLine(const char* text, bool reset = false) {
+	static int y = 5;
+	if (reset) {
+		y = 5;
+		return;
+	}
+	DrawText(text, 5, y, 20, WHITE);
+	y += 20;
+}
+void DebugLine(Vector2 v) { DebugLine(TextFormat("%f %f", v.x, v.y)); }
+void DebugLine(float v) { DebugLine(TextFormat("%f", v)); }
 
 int main(int argc, char** argv) {
-    // Define window dimensions
-    const int window_width = 800;
-    const int window_height = 450;
+	// Define window dimensions
+	const int window_width = 1600;
+	const int window_height = 900;
 
-    // Create window
-    InitEngine(window_width, window_height, "Raylib Example");
-    SetTargetFPS(60);
+	// Create window
+	InitEngine(window_width, window_height, "Raylib Example");
+	SetTargetFPS(60);
 
-    // Create scene
-    SolidBox* box = new SolidBox();
-    box->size = {50.0f,50.0f};
-    box->setPosition(400.0f,225.0f);
-    box->reparent(scene);
+	// Create scene
+	Sprite2D* ethan = new Sprite2D();
+	ethan->size = { 100,100 };
+	ethan->setPosition(800, 450);
+	ethan->setTexture("../../../assets/ethan.jpg");
+	ethan->reparent(scene);
 
-    CollisionBox* collision = new CollisionBox();
-    collision->size = box->size;
-    collision->reparent(box);
+	CollisionBox* ethan_collision = new CollisionBox();
+	ethan_collision->size = ethan->size;
+	ethan_collision->reparent(ethan);
 
-    // Initialize scene tree
-    ReadyTree();
+	SolidCircle* roland = new SolidCircle();
+	roland->radius = 40.0f;
+	roland->setPosition(800, 450);
+	roland->reparent(scene);
+	
+	CollisionCircle* roland_collision = new CollisionCircle();
+	roland_collision->radius = roland->radius;
+	roland_collision->reparent(roland);
 
-    // Main loop
-    while(!WindowShouldClose()) {
-        UpdateTree(); // Run _update() for every element in tree
+	// Initialize scene tree
+	ReadyTree();
 
-        BeginDrawing();
-        ClearBackground(DARKGRAY);
-        DrawTree();
-        DrawCircleV(collision->closestPoint(Mouse.getPosition()),3.0f,BLUE);
-        DrawFPS(5, 5);
-        EndDrawing();
-    }
+	// Main loop
+	while (!WindowShouldClose()) {
+		UpdateTree(); // Run _update() for every element in tree
+		ethan->rotate(1);
+		roland->setPosition(Mouse.getPosition());
+		roland->rotate(Mouse.getWheel() * 10);
 
-    // Clean up
-    Quit();
+		BeginDrawing();
+		ClearBackground(DARKGRAY);
+		DrawTree();
+		DebugLine("", true);
+		if (roland_collision->overlapping(ethan_collision)) {
+			DebugLine("Overlap");
+		}
+		//DrawFPS(5, 5);
+		EndDrawing();
+	}
+
+	// Clean up
+	Quit();
 
 
-    return 0;
+	return 0;
 }
